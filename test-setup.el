6711d59a3912d799271122b7d23aa7acb7103433
Duncan Bayne <duncan@bayne.id.au>
2018-06-28 21:33:23 +1000

    Perhaps we have to add to package list before initializing

--- test-setup.el
+++ test-setup.el
@@ -1,6 +1,7 @@
 (require 'package)
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
 (package-initialize)
-(add-to-list 'package-archives (cons "melpa" "https://melpa.org/packages/"))
 (package-refresh-contents)
 (mapcar (lambda (package-name) (package-install package-name)) '(elnode org-present))
 
