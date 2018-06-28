ca54a70a85a2f4120e410049088824217a11452e
Duncan Bayne <duncan@bayne.id.au>
2018-06-28 21:14:20 +1000

    Install elnode and org-present packages before running tests

--- bin/ci.sh
+++ bin/ci.sh
@@ -5,4 +5,5 @@ set -e
 apt update -qq
 apt install -y emacs-nox
 
+emacs --batch --eval "(progn (require 'package) (package-initialize) (package-install 'elnode))"
 emacs -batch -l ert -l org-present-remote.el -l unit-tests.el -l integration-tests.el -f ert-run-tests-batch-and-exit
