7dac94285d02add053c99cc9170af02899847d55
Duncan Bayne <duncan@bayne.id.au>
2018-06-28 21:23:34 +1000

    Fix typo in cons

--- test-setup.el
+++ test-setup.el
@@ -1,4 +1,4 @@
 (require 'package)
 (package-initialize)
-(add-to-list 'package-archives (cons "melpa" "https://melpa.org/packages/" t))
+(add-to-list 'package-archives (cons "melpa" "https://melpa.org/packages/"))
 (package-install 'elnode 'org-present)
