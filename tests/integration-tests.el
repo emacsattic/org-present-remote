0498e76e7f1f9789419c4bb14723f7268232e518
Duncan Bayne <duncan@bayne.id.au>
2018-07-01 10:52:34 +1000

    Add first attempt at integration test that checks page contents

--- tests/integration-tests.el
+++ tests/integration-tests.el
@@ -1,3 +1,19 @@
+(defun buffer-contains-substring (string)
+  (save-excursion
+    (save-match-data
+      (goto-char (point-min))
+      (search-forward string nil t))))
+
 (ert-deftest skeleton-integration-test ()
-  "Test that always passes, to exercise GitLab CI."
-  (should (equal "foo" "foo")))
+  (org-present-remote/remote-off)
+
+  (when (get-buffer "test.org")
+    (kill-buffer "test.org"))
+
+  (with-current-buffer (find-file "tests/fixtures/test.org")
+    (org-present)
+    (org-present-remote/remote-on "localhost"))
+
+  (switch-to-buffer (url-retrieve-synchronously "http://localhost:8009/"))
+  (should (buffer-contains-substring "<h1>test.org</h1>"))
+  (should (buffer-contains-substring "<h2>heading 1</h2>")))
--- tests/test-setup.el
+++ tests/test-setup.el
@@ -1,7 +1,13 @@
 (require 'package)
+
+(defvar org-present-remote/package-dependencies
+  '(elnode org-present esxml))
+
 (add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/"))
 (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
+
 (package-initialize)
 (package-refresh-contents)
-(mapcar (lambda (package-name) (package-install package-name)) '(elnode org-present))
+
+(mapcar (lambda (package-name) (package-install package-name)) org-present-remote/package-dependencies)
 
