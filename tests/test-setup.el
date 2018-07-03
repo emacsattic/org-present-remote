ed20dd43316ae852ef3542e464f1a0fe12b27eba
Duncan Bayne <duncan@bayne.id.au>
2018-07-03 17:24:46 +1000

    Okay, I think this is the correct order

--- tests/test-setup.el
+++ tests/test-setup.el
@@ -3,10 +3,12 @@
 (defvar org-present-remote/package-dependencies
   '(elnode org-present))
 
+(package-initialize)
+
 (setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
                          ("marmalade" . "https://marmalade-repo.org/packages/")
                          ("melpa" . "https://melpa.org/packages/")))
-(package-initialize)
+
 (package-refresh-contents)
 
 (mapcar (lambda (package-name) (package-install package-name)) org-present-remote/package-dependencies)
