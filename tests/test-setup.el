9e71749ea4c5981742bcbf56b4e0f37f106fbde9
Duncan Bayne <duncan@bayne.id.au>
2018-07-03 17:22:22 +1000

    Must package-refresh-contents come before package-initialize?

--- tests/test-setup.el
+++ tests/test-setup.el
@@ -6,8 +6,9 @@
 (setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
                          ("marmalade" . "https://marmalade-repo.org/packages/")
                          ("melpa" . "https://melpa.org/packages/")))
-(package-initialize)
+
 (package-refresh-contents)
+(package-initialize)
 
 (mapcar (lambda (package-name) (package-install package-name)) org-present-remote/package-dependencies)
 
