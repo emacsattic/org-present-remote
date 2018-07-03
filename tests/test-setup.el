e62912324fca53aa20e85a8818509e4a5345ac46
Duncan Bayne <duncan@bayne.id.au>
2018-07-03 17:29:41 +1000

    Let's just quietly remove Marmalade and see if stuff works

--- tests/test-setup.el
+++ tests/test-setup.el
@@ -6,7 +6,6 @@
 (package-initialize)
 
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-                         ("marmalade" . "http://marmalade-repo.org/packages/")
                          ("melpa" . "http://melpa.org/packages/")))
 
 (package-refresh-contents)
