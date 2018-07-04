084ca24b2a7733d5718e28109d196763992e566b
Duncan Bayne <duncan@bayne.id.au>
2018-07-04 16:48:32 +1000

    Let's stick with integration tests for now

--- bin/ci.sh
+++ bin/ci.sh
@@ -11,6 +11,5 @@ emacs \
     -l tests/test-setup.el \
     -l ert \
     -l org-present-remote.el \
-    -l tests/unit-tests.el \
     -l tests/integration-tests.el \
     -f ert-run-tests-batch-and-exit
--- tests/unit-tests.el
+++
@@ -1,3 +0,0 @@
-(ert-deftest skeleton-unit-test ()
-  "Test that always passes, to exercise GitLab CI."
-  (should (equal "foo" "foo")))
