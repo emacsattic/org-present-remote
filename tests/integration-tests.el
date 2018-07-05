d3bd3be332e7ca261a723f8243f6cf77f44ccbe7
Duncan Bayne <duncan@bayne.id.au>
2018-07-05 22:58:15 +1000

    Remove logging from integration test

--- tests/integration-tests.el
+++ tests/integration-tests.el
@@ -14,17 +14,11 @@
     (org-mode)
     (insert "* heading 1\n* heading 2")
     (goto-char (point-min))
-    (print ">>> test.org")
     (print (buffer-string))
-    (print "<<< test.org")
     (org-present)
     (org-present-remote/remote-on "127.0.0.1"))
 
-  (print "attempting to download page")
-
   (with-current-buffer (url-retrieve-synchronously "http://127.0.0.1:8009/" :silent nil)
-    (print ">>> HTML")
     (print (buffer-string))
-    (print "<<< HTML")
     (should (buffer-contains-substring "<h1>test.org</h1>"))
     (should (buffer-contains-substring "<h2>heading 1</h2>"))))
