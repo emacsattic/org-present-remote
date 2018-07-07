5ebc8bafc7e87c3d98003dcf913cb13e02163e47
Duncan Bayne <duncan@bayne.id.au>
2018-07-07 22:00:43 +1000

    Revert "Try splitting lint steps to fail on errors"
    This reverts commit b9843387f051fd6ac131070be4396a22e7dbadd0.

--- tests/lint-tests.el
+++ tests/lint-tests.el
@@ -3,7 +3,5 @@
 (require 'package-lint)
 
 (with-current-buffer (find-file "org-present-remote.el")
-  (checkdoc-current-buffer))
-
-(with-current-buffer (find-file "org-present-remote.el")
+  (checkdoc-current-buffer)
   (package-lint-current-buffer))
