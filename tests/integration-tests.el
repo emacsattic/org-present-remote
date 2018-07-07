1d1aa74e8736fb8080eac425c3914e0c5a2ad443
Duncan Bayne <duncan@bayne.id.au>
2018-07-07 21:03:50 +1000

    Fix integration tests to use new namespace idiom

--- tests/integration-tests.el
+++ tests/integration-tests.el
@@ -5,7 +5,7 @@
       (search-forward string nil t))))
 
 (ert-deftest skeleton-integration-test ()
-  (org-present-remote/remote-off)
+  (org-present-remote--remote-off)
 
   (when (get-buffer "test.org")
     (kill-buffer "test.org"))
@@ -15,7 +15,7 @@
     (insert "* heading 1\n* heading 2")
     (goto-char (point-min))
     (org-present)
-    (org-present-remote/remote-on "127.0.0.1"))
+    (org-present-remote--remote-on "127.0.0.1"))
 
   (with-current-buffer (url-retrieve-synchronously "http://127.0.0.1:8009/" :silent nil)
     (should (buffer-contains-substring "<h1>test.org</h1>"))
@@ -28,3 +28,4 @@
   (with-current-buffer (url-retrieve-synchronously "http://127.0.0.1:8009/prev" :silent nil)
     (should (buffer-contains-substring "<h1>test.org</h1>"))
     (should (buffer-contains-substring "<h2>heading 1</h2>"))))
+
