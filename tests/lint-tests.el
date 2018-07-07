dc5d13b6693bbd321594f18f267d6f15e8a630b6
Duncan Bayne <duncan@bayne.id.au>
2018-07-07 21:21:50 +1000

    Look in the right directory for the file to lint

--- tests/lint-tests.el
+++ tests/lint-tests.el
@@ -1,6 +1,6 @@
 (require 'checkdoc)
 (require 'elnode)
 
-(with-current-buffer (find-file "../org-present-remote.el")
+(with-current-buffer (find-file "org-present-remote.el")
   (checkdoc-current-buffer t)
   (package-lint-current-buffer))
